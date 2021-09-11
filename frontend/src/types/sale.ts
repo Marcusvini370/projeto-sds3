b1d1189731597c6df279e546a63ddcfda767b4d6
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-11 16:12:26 -0300

    First request

---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,33 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[]; //ele é do tipo lista/ array Sale dentro dele é uma lista de sale
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    first: boolean;
+    number: number;
+    numberOfElements?: number;
+    empty?: boolean;
+  }
+  
+  export type SaleSum = {
+    sellerName: string, //tipo 
+    sum: number, //tipo numerico
+  }
+  
+  export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+  }
