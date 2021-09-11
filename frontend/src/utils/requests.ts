8e0386eda0f8c0aa4526b763abd02750ab8c8b5a
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-11 17:39:57 -0300

    teste f

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
 //nossa api banco de dados
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds4-marcus.herokuapp.com/'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-dev-spring-react.herokuapp.com'
 
 //significa que a baseurl da sua aplicação vai ser pra pegar a variavel que botamos no netfly esse react_app e de lá se estiver definidada ele vai pegar o backend se n tiver definida ela vai pegar o lado oposto. 
