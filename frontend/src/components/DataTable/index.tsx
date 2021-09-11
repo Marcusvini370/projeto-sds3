f2f5cc9b4f82dbc28dd0e4beddc364f527b75c61
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-11 16:19:56 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,68 +1,68 @@
+import axios from "axios";
+import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+    //n precisa parametizar pq vai ser um numero ent deixa so de valor 0 inicial
+    const [activePage, setActivePage] = useState(0);
+
+    const [page, setPage] = useState<SalePage>({ 
+        first: true, //primeira pagina
+        last: true,  //ultima pagina 
+        number: 0, //numeor pagina 0
+        totalElements: 0, //elementos 0
+        totalPages: 0 //pagina 0
+
+    });
+   
+     useEffect(() => { //vamos passar nossa ativação de pagina aqui onde era o numero da página
+        axios.get(`${ BASE_URL}/sales?page=${activePage}&size=20sort=date,desc`)
+        .then(response =>  {
+            setPage(response.data); 
+        }) 
+
+     }, [activePage]) //ta observado o activePage
+
+    
+     const changePage = (index: number) => {
+         
+        setActivePage(index); 
+     }
+
   return (
+      <>
+      <Pagination page={page} onPageChange={changePage} />
     <div className="table-responsive">
-      <table className="table table-striped table-sm">
+    <table className="table table-striped table-sm">
         <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
+            <tr>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes visitados</th>
+                <th>Negócios fechados</th>
+                <th>Valor</th>
+            </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
+            {page.content?.map(item => (
+                <tr key={item.id}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+            </tr>
+            )) }
+            
+           
         </tbody>
-      </table>
-    </div>
-  );
-};
+    </table>
+ </div>
 
+  </>
+    );
+}
 export default DataTable;
--- frontend/src/types/seller.ts
+++ frontend/src/types/seller.ts
@@ -1,4 +1,14 @@
+import { format } from 'date-fns';
+
 export type Seller = {
     id: number;
     name: string
+  }
+
+  //função para arredonda o numero em 1 casa decila etc
+
+export const formatLocalDate = (date: string, pattern: string) => {
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, pattern);
   }
